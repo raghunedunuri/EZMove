b72461c916dc1a76aab4c12ee4d3835fb6c9f560
raghunedunuri <raghu.nedunuri@gmail.com>
2018-01-18 12:54:27 +0530

    Added blocked js files

---
+++ EzMove/Scripts/app/app.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="../angular.js" />
+var ezmoveapp = angular.module('ezmoveapp', ['ui.router', 'xeditable', 'ui.bootstrap']);
+ezmoveapp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+        // Tabs states
+        .state('home', {
+            url: '/',
+            templateUrl: location.protocol + '//' + location.host + "/Scripts/app/partials/Default.html"
+        })
+        .state('Request', {
+            url: '/Request',
+            templateUrl: location.protocol + '//' + location.host + "/Scripts/app/partials/Request.html"
+        })
+        .state('Communication', {
+            url: '/Communication',
+            templateUrl: location.protocol + '//' + location.host + "/Scripts/app/partials/Communication.html"
+        })
+        .state('employees', {
+            url: '/employees',
+            templateUrl: location.protocol + '//' + location.host + "/Scripts/app/partials/Employee/List.html"
+        })
+        .state('employees.details', {
+            url: '/details',
+            templateUrl: location.protocol + '//' + location.host + "/Scripts/app/partials/Employee/Details.html"
+        })
+        .state('employees.details.edit', {
+            url: '/edit',
+            templateUrl: location.protocol + '//' + location.host + "/Scripts/app/partials/Employee/Edit.html"
+        });
+
+})
