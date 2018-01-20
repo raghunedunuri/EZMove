dda975f1cc90c375248167c3c9eb06f33819aea6
raghunedunuri <raghu.nedunuri@gmail.com>
2018-01-20 23:02:31 +0530

    Com

---
+++ EzMove/Scripts/app/controllers/employeecontroller.js
@@ -0,0 +1,36 @@
+﻿ezmoveapp.controller('employeecontroller', ['$scope', employeecontroller]);
+
+function employeecontroller($scope) {
+    $scope.users = [
+    {
+        firstname: "Raghu",
+        lastname: "Nedunuri",
+        email: "raghu.nedunuri@gmail.com"
+    },
+    {
+        firstname: "Srinivas",
+        lastname: "Mundru",
+        email: "srinivas.mundru@gmail.com"
+    },
+    {
+        firstname: "Ayyappa",
+        lastname: "Yellapu",
+        email: "ayyappa.yellapu@gmail.com"
+    }
+    ]
+
+    $scope.checkName = function (data, id) {       
+    };
+
+    $scope.saveUser = function (data, id) {
+        //$scope.user not updated yet       
+        angular.extend(data, { id: id });
+        return $http.post('/saveUser', data);
+    };
+
+    // remove user
+    $scope.removeUser = function (index) {        
+        $scope.users.splice(index, 1);
+    };
+
+}
---
+++ EzMove/Scripts/app/services/employeeservice.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="../app.js" />
+ezmoveapp.factory('employeeservice', ['$resource', employeeservice]);
+
+function employeeservice($resource) {
+    return $resource(location.protocol + '//' + location.host + "/Api/Dashboard", {}, {
+        getTLBInfo: {
+            url: location.protocol + '//' + location.host + "/Api/Dashboard/GetTLBInfo",
+            isArray: true
+        },
+        getOtherTLBInfo: {
+            url: location.protocol + '//' + location.host + "/Api/Dashboard/GetOtherTLBInfo",
+            isArray: true
+        }
+    });
+}
